a208020afe908f95e35a6f43348f97366e879ec9
RuslanFeiskhanov <99216168+RuslanFeiskhanov@users.noreply.github.com>
2022-05-18 21:48:30 +0300

    Nill Kiggers

--- include/bst.h
+++ include/bst.h
@@ -2,4 +2,47 @@
 #ifndef INCLUDE_BST_H_
 #define INCLUDE_BST_H_
 
+template<typename T>
+
+class BST {
+ private:
+  struct Node {
+    T val;
+    Node *right;
+    Node *left;
+    int count;
+};
+
+Node *root;
+Node *addNode(Node *root, T x) {
+    if (root == nullptr) {
+        root = new Node;
+        root->val = x;
+        root->left = nullptr;
+        root->right = nullptr;
+    } else {
+        if (root->val > x) root->left = addNode(root->left, x);
+        if (root->val < x) root->right = addNode(root->right, x);
+        if (root->val == x) (root->count)++;
+    }
+    return root;
+  }
+
+int value(Node* root, T x) {
+    if (root == nullptr) {
+      return 0;
+    } else {
+        if (root->val == x) return root->count;
+        if (root->val > x) return searchValue(root->left, x);
+        if (root->val < x) return searchValue(root->right, x);
+    }
+}
+
+ public:
+  BST() : root(nullptr) {}
+  void add(T x) {
+    root = addNode(root, x);
+  }
+};
+
 #endif  // INCLUDE_BST_H_
