ec3fecca8d1e6cc0447caa1ac3a2ad677e3d95ec
RuslanFeiskhanov <99216168+RuslanFeiskhanov@users.noreply.github.com>
2022-05-18 23:06:28 +0300

    h

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,6 +6,7 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
+
     std::ifstream file(filename);
     BST<std::string> tree;
     std::string word = "";
@@ -13,13 +14,12 @@ BST<std::string> makeTree(const char* filename) {
 // читаем содержимое файла посимвольно
     while (!file.eof()) {
         char ch = file.get();
-        if (ch >= 'a' && ch <= 'z') {
+        if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
             if (ch >= 'A' && ch <= 'Z') ch += 32;
             word += ch;
             count++;
-        }
+        } else if (count > 0) { tree.add(word); word = "";  count = 0;}
     }
-    if (count > 0) { tree.add(word); word = "";  count = 0;}
 
     file.close();
     return tree;
