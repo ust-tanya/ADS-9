5e18394ada31909f4b2f9b03b4289d4c1bb345ab
ust-tanya <94698360+ust-tanya@users.noreply.github.com>
2022-05-20 23:50:58 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,5 +6,22 @@
 #include  "bst.h"
 
 BST<std::string> makeTree(const char* filename) {
-  // поместите сюда свой код
+  std::ifstream file(filename);
+  BST<std::string> bst;
+  std::string wrd = "";
+  int c;
+// читаем содержимое файла посимвольно
+  while (!file.eof()) {
+  char sym = file.get();
+  if ((sym >= 'a' && sym <= 'z') || (sym >= 'A' && sym <= 'Z')) {
+  if (sym >= 'A' && sym <= 'Z') 
+  sym += 32;
+  wrd += sym;
+  c++;
+  } else if (c != 0) {
+  wrd = "";  c = 0; bst.add(wrd);
+  }
+  }
+  file.close();
+  return bst;
 }
