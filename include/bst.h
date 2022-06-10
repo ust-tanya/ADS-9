6b8d9ba626d14259e642f62d12f98d8df2fc2f60
ust-tanya <94698360+ust-tanya@users.noreply.github.com>
2022-06-10 21:22:52 +0300

    Update bst.h

--- include/bst.h
+++ include/bst.h
@@ -3,62 +3,63 @@
 #define INCLUDE_BST_H_
 
 template<typename T>
+
 class BST {
  private:
   struct Node {
-  T data;
-  Node *r;
-  Node *l;
-  int count;
+    T val;
+    Node *rght;
+    Node *lft;
+    int count;
   };
-  Node *root;
-  Node *addNode(Node *root, T val) {
-  if (root == nullptr) {
-  root = new Node;
-  root->data = val;
-  root->l = nullptr;
-  root->r = nullptr;
-  } else {
-  if (root->data > val) root->l = addNode(root->l, val);
-  if (root->data < val) root->r = addNode(root->r, val);
-  if (root->data == val) (root->count)++;
-  }
-  return root;
+  Node *rt;
+  Node *addNode(Node *rt, T x) {
+      if (rt == nullptr) {
+          rt = new Node;
+          rt->val = x;
+          rt->lft = nullptr;
+          rt->rght = nullptr;
+      } else {
+          if (rt->val > x) rt->lft = addNode(rt->lft, x);
+          if (rt->val < x) rt->rght = addNode(rt->rght, x);
+          if (rt->val == x) (rt->count)++;
+      }
+      return rt;
+    }
+
+  int searchValue(Node* rt, T x) {
+    if (rt == nullptr) {
+      return 0;
+    } else if (rt->val == x) {
+        return rt->count;
+    } else if (rt->val < x) {
+        return searchValue(rt->rght, x);
+    } else {
+        return searchValue(rt->lft, x);
+    }
   }
 
-  int SearchNode(Node* root, T val) {
-  if (root == nullptr) {
-  return 0;
-  } else if (root->data == val) {
-  return root->count;
-  } else if (root->data < val) {
-  return SearchNode(root->r, val);
-  } else {
-  return SearchNode(root->l, val);
-  }
-  }
-
-  int TreeHeight(Node* root) {
-  if (root == nullptr)
-  return 0;
-  if (root->l == nullptr && root->r == nullptr) return 0;
-  int rt = TreeHeight(root->r), lt = TreeHeight(root->l);
-  if (rt > lt)
-  return rt + 1;
-  else
-  return lt + 1;
+  int dpth(Node* rt) {
+      if (rt == nullptr)
+        return 0;
+      if (rt->lft == nullptr && rt->rght == nullptr) return 0;
+      int rt = dpth(rt->rght), lt = dpth(rt->lft);
+      if (rt > lt)
+        return rt + 1;
+      else
+        return lt + 1;
   }
 
  public:
-  BST():root(nullptr) {}
-  void add(T val) {
-  root = addNode(root, val);
+  BST():rt(nullptr) {}
+  void add(T x) {
+      rt = addNode(rt, x);
   }
   int depth() {
-  return TreeHeight(root);
+      return dpth(rt);
   }
-  int search(T val) {
-  return SearchNode(root, val) + 1;
+  int search(T x) {
+      return searchValue(rt, x) + 1;
   }
 };
 
